b5643a9a09dbf6c7842ab339c79ab9a290f3cc08
Diana Shimlykh <d56671250@gmail.com>
2026-02-11 14:48:50 +0300

    add: two formats of getDate

--- script.js
+++ script.js
@@ -1,115 +1,100 @@
 "use strict";
 
-const appData = {
-  title: "",
-  screens: [],
-  screenPrice: 0,
-  adaptive: true,
-  rollback: 10,
-  services: {},
-  allServicePrices: 0,
-  fullPrice: 0,
-  servicePercentPrice: 0,
+function getCurrentDate() {
+  const today = new Date();
 
-  start: function () {
-    this.asking();
-    this.addPrices();
-    this.getFullPrice();
-    this.getServicePercentPrices();
-    this.getTitle();
-    this.logger();
-  },
-   isNum: function (num) {
-    return !isNaN(num) && isFinite(num);
-  },
-    isString: function (str) {
-      return typeof str === 'string' && str.trim() !== '' && isNaN(str);
-  },
-    asking: function() {
-      do {
-        this.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-      } while (!this.isString(this.title));
-    
- 
-     for (let i = 0; i < 2; i++){
-      let name;
-      let price = 0;
+  const day = today.getDate();
+  const month = today.getMonth(); //0 - 11
+  const year = today.getFullYear();
 
-      do {
-        name = prompt ("Какие типы экранов нужно разработать?", "Простые, Сложные");
-      } while ((!this.isString(name)));
+  const hours = today.getHours();
+  const min = today.getMinutes();
+  const sec = today.getSeconds();
 
-      do {
-      price = prompt("Сколько будет стоить эта работа?");
-      } while (price === null || !this.isNum(+price));
+  const weekDay = today.getDay();
 
-      this.screens.push({id: i, name: name, price: price});
-     }
+  const weekdays = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+  ];
+  const months = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+  ];
 
-    for (let i = 0; i < 2; i++) {
-      let price;
-      let name;
+  return {                //easier to get access to resources via objects - instead of day + " " + month ...
+    day,
+    month: months[month],
+    year,
+    hours,
+    min,
+    sec,
+    weekDay: weekdays[weekDay],
+  };
+}
 
-      do {
-      name = prompt("Какой дополнительный тип услуги нужен?");
-      if (name === null) return;
-      } while (!this.isString(name));
+function changeEnding(num, forms) { //to calcualte 
+  num = Math.abs(num) % 100;
+  const n1 = num % 10;
 
-      do {
-        price = prompt("Сколько это будет стоить?");
-      } while (price === null || !this.isNum(+price));
-     
-      if(this.services[name]){
-        this.services[name].push(+price);
-      } else {
-        this.services[name] = [+price];
-      }
-    }
+  if (num > 10 && num < 20) return forms[2];
+  if (n1 > 1 && n1 < 5) return forms[1];
+  if (n1 === 1) return forms[0];
+  return forms[2];
+}
+const d = getCurrentDate(); // toISOString() часто применяют при работе с API и сохранении дат в БД, поскольку он независим от часового пояса. нужно ли использовать? 
+const getDateVar1 = `Сегодня ${d.weekDay}, ${d.day} ${d.month} ${d.year} года, ${d.hours} ${changeEnding(d.hours, ["час", "часа", "часов"])} ${d.min} ${changeEnding(d.min, ["минута", "минуты", "минут"])} ${d.sec} ${changeEnding(d.sec, ["секунда", "секунды", "секунд"])}`;
 
-    this.screenPrice = parseFloat(this.screenPrice);
-    this.adaptive = confirm("Нужен ли адаптив на сайте?");
-  },
 
-  addPrices: function () {
-  this.screenPrice = this.screens.reduce((sum, screen) => {
-    return sum + +screen.price;
-  }, 0);
+//  function getCurrentDateNum() {        //classical variant?
+//   function pad(num) {
+//   return num < 10 ? '0' + num : String(num);
+// }
+//   const now = new Date();
 
-  this.allServicePrices = Object.values(this.services).reduce((sum, arr) => {
-      return sum + arr.reduce((first, second) => first + second, 0);
-    }, 0);
-},
-  
-  getFullPrice: function () {
-    this.fullPrice = this.screenPrice + this.allServicePrices;
-  },
-  getServicePercentPrices: function () {
-    this.servicePercentPrice = this.fullPrice - this.fullPrice * (this.rollback / 100);
-  },
-  getTitle: function () {
-    let goodTitle = this.title.trim().toLowerCase();
-    this.title = goodTitle[0].toUpperCase() + goodTitle.slice(1);
-  },
-  getRollbackMessage: function (price) {
-    if (price >= 30000) {
-      return "Даем скидку в 10%";
-    } else if (price  >= 15000 && price < 30000) {
-      return "Даем скидку в 5%";
-    } else if (price  >= 0  && price < 15000) {
-      return "Скидка не предусмотрена.";
-    } else if (price < 0) {
-      return "Что-то пошло не так.";
-    }
-  },
-  logger: function () {
-    // for (let key in this) {. //for all info in the programm
-    //     console.log(key, this[key]);
-    // }
-    console.log(appData.fullPrice);
-    console.log(appData.servicePercentPrice);
-    console.log(this.screens);
-    
-  }
-};
+//   const day = pad(now.getDate());
+//   const month = pad(now.getMonth() + 1);
+//   const year = now.getFullYear();
 
-appData.start();
+//   const hours = pad(now.getHours());
+//   const minutes = pad(now.getMinutes());
+//   const seconds = pad(now.getSeconds());
+
+//   return `${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`;
+// }
+
+function getCurrentDateNum() {
+  const today = new Date();
+
+  const formatter = new Intl.DateTimeFormat('ru-RU', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+
+  return formatter.format(today).replace(',', ' -');
+}
+
+setInterval(()=> {
+  console.log(getCurrentDateNum(), 1000)
+});
+
+console.log(getDateVar1);
