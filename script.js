0e94fe05ede258b722d1a723c787bed4b47c4afe
Diana Shimlykh <d56671250@gmail.com>
2026-02-14 02:30:46 +0300

    add: listeners task solution

--- script.js
+++ script.js
@@ -1,39 +1,21 @@
 "use strict";
-const polishBookLibrary = function() {
-  let books = document.querySelectorAll(".book");
-  const misprint = books[4].querySelector('h2 a');
-  const addChapter = document.createElement('li');
-  
-const restoreOrder = function () {
-  const book6 = books[2].querySelector('ul').append(addChapter);
-  const chapters2 = books[0].querySelectorAll("li");
-  const chapters5 = books[5].querySelectorAll("li");
-  const chapters6 = books[2].querySelectorAll("li");
 
-  books[0].before(books[1]);
-  books[3].before(books[4]);
-  books[5].after(books[2]);
+const button = document.querySelector('#btn');
+const input = document.querySelector('#text');
+const square = document.querySelector('#square')
+const circle = document.querySelector('#circle');
+const innerBtn = document.querySelector('#e_btn');
+const range = document.querySelector('#range');
 
-  chapters2[3].after(chapters2[6]);
-  chapters2[6].after(chapters2[8]);
-  chapters2[9].after(chapters2[2]);
+button.addEventListener('click', (e) => {
+   square.style.backgroundColor = input.value.trim();
+   input.value = '';
+   innerBtn.style.display = 'none';
+})
 
-  chapters5[2].before(chapters5[9]);
-  chapters5[4].after(chapters5[2]);
-  chapters5[7].after(chapters5[5]);
+range.addEventListener('input', () => {
+    const value = range.value;
 
-
-  console.log(chapters6[10])
-  chapters6[8].after(chapters6[10]);
-};
-restoreOrder();
-  document.querySelector('.adv').remove();
-  misprint.innerHTML = 'Книга 3. this и Прототипы Объектов';
-  document.body.style.backgroundImage = 'url("image/you-dont-know-js.jpg")';
-  addChapter.textContent = 'Глава 8: За пределами ES6';
-
-}
-polishBookLibrary();
-
-
- 
+    circle.style.width = value + "%";
+    circle.style.height = value + "%";
+})
